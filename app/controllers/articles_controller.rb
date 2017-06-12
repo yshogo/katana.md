6ab7e6055bfb1d39378788e9e13d1c01bc7e512c
shogo.yamada <superman199323@gmail.com>
2017-06-13 07:42:52 +0900

    最初のコミット

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,10 @@
+class ArticlesController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'articles/show'
+
+  get 'articles/new'
+
+  get 'articles/edit'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
